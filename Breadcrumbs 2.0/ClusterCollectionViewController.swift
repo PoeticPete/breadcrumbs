044d0bd02a8baa595b1c4b3348ef7e4790fe9e83
Peter Tao <petertao@1x-nat-vl930-172-30-79-80.wireless.umass.edu>
2016-11-27 16:36:39 -0500

    supppppp

--- Breadcrumbs 2.0/ClusterCollectionViewController.swift
+++ Breadcrumbs 2.0/ClusterCollectionViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ClusterCollectionViewController: UIViewController {
+class ClusterCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var clusteredAnnotations = [CustomAnnotation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +25,54 @@ class ClusterCollectionViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return clusteredAnnotations.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width/3 - 0
+        
+        return CGSize(width: width, height: width)
+    }
+    
+    
+    
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let annotationPost = clusteredAnnotations[indexPath.row].post!
+        if annotationPost.hasPicture == true {
+            var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoClusterCell", for: indexPath) as! PhotoCollectionViewCell
+            cell.photo.image = getImageFromURL(annotationPost.mediaURL!)
+            
+            return cell
+        } else {
+            var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MessageClusterCell", for: indexPath) as! TextClusterCollectionViewCell
+            cell.messageLabel.text = annotationPost.message
+            
+            return cell
+        }
 
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ClusterCollectionToCrumbSegue", sender: indexPath.row)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ClusterCollectionToCrumbSegue" {
+            var nextVC = segue.destination as! CrumbTableViewController
+            let indexPath = sender as! Int
+            nextVC.annotation = clusteredAnnotations[indexPath]
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
